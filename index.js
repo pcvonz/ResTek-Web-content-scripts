9ce8e38feab44aad13f64679aa3d6f2a82b7d599
paul[1~ <pcvonz@gmail.com>
2016-01-14 00:10:54 +0000

    Still piecing throught utorials

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+var { ToggleButton } = require('sdk/ui/button/toggle');
+var panels = require("sdk/panel");
+var self = require("sdk/self");
 var buttons = require('sdk/ui/button/action');
 var tabs = require("sdk/tabs");
 
-var button = buttons.ActionButton({
+var button = ToggleButton({
   id: "content_scripts",
   label: "Opens up the housing website on live/dev",
   icon: {
@@ -9,12 +12,30 @@ var button = buttons.ActionButton({
     "32": "./icon-32.png",
     "64": "./icon-64.png"
   },
-  onClick: handleClick
+  //onClick: handleClick,
+  onChange: handleChange
 });
+
+var panel = panels.Panel({
+	contnetURL: self.data.url("panel.html"),
+	onHide: handleHide
+});
+
+function handleChange(state) {
+	if (state.checked) {
+		panel.show({
+			position: button
+		});
+	}
+}
+
+function handleHide() {
+	button.state('window', {checked: false});
+}
 
 function handleClick(state) {
 	current = tabs.activeTab.url;
-	if (current.slice(0, 11) == 'http://nari') {
+	if ((current.slice(0, 11) == 'http://nari') || (current.slice(0, 4) == 'nari')) {
 		current = current.replace('nari.', '');
 		current = current.replace('~vonzimp', 'apps');
 } else {
